b4ed4b807a7ff920f054f44ee4a89da77538aa5e
Adrian Barnutiu <barnutiu@yahoo.com>
2025-08-22 23:32:17 +0300

    new commit

--- e2e/registerUser.spec.ts
+++ e2e/registerUser.spec.ts
@@ -4,5 +4,16 @@ test('register user', async ({page}) => {
     await page.goto('https://www.automationexercise.com/');
     await expect(page).toHaveTitle('Automation Exercise');
     await page.getByRole('button', {name: "Consent"}).click();
-    await page.getByText(' Signup / Login').click(); 
+    await page.getByText(' Signup / Login').click();
+    
+    const email1 = page.locator('.login-form')
+    await expect(email1).toBeVisible();
+    await email1.getByPlaceholder("Email Address").fill("test@test.com")
+
+    const email2 = page.locator(('.signup-form'))
+    await expect(email2).toBeVisible();
+    await email2.getByPlaceholder("Email Address").fill("test2@test.com")
+
+
+
 })
